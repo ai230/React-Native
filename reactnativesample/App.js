056ab3cc48c3e1bbb8ca49442704a5a489386e98
AiYamamoto <ay36230@gmail.com>
2018-02-25 18:56:17 -0800

    initial commit

---
+++ reactnativesample/App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import TodoInput from './src/component/TodoInput';
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <TodoInput />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    paddingTop: 40,
+    alignItems: 'center',
+  },
+  main: {
+    flex: 1,
+    maxWidth: 400,
+    alignItems: 'center',
+  }
+});
