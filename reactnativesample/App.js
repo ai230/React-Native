5ee043c090603b847885dc915f2443129536204f
AiYamamoto <ay36230@gmail.com>
2018-02-27 05:20:04 -0800

    Add TodoItem.js

--- reactnativesample/App.js
+++ reactnativesample/App.js
@@ -1,31 +1,68 @@
 /**
- * Sample React Native App
+ * Sample React Native App 
  * https://github.com/facebook/react-native
  * @flow
  */
 
+// (SFC:Stateless Functional Component)
+// パフォーマンスが良い。リスト項目の一アイテム
+// class構文のように内部メソッドを持たせられない
+// stateを持つことができない
+// componentWillMountといったライフサイクルイベントが使えない
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList,//渡す配列にはkeyという一意な項目が必ず必要になる
 } from 'react-native';
 import TodoInput from './src/component/TodoInput';
+import TodoItem from './src/component/TodoItem';
 
 
 export default class App extends Component<{}> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [],
+    };
+  }
+
   _onPress = (text) => {
     console.log(text);
+    const list = [].concat(this.state.list);
+
+    list.push({
+      key: Date.now(),
+      text: text,
+      done: false,
+    });
+
+    this.setState({
+      list,
+    });
   }
 
   render() {
+    const {
+      list,
+    } = this.state;
+
     return (
       <View style={styles.container}>
         <View style={styles.main}>
           <TodoInput onPress={this._onPress} />
+          <View style={styles.todoListContainer}>
+            <FlatList
+              style={styles.todoList}
+              data={list}
+              renderItem={({ item }) => <TodoItem {...item} />}
+            />
         </View>
       </View>
+    </View>
     );
   }
 }
@@ -41,5 +78,13 @@ const styles = StyleSheet.create({
     flex: 1,
     maxWidth: 400,
     alignItems: 'center',
+  },
+  todoListContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  todoList: {
+    paddingLeft: 10,
+    paddingRight: 10,
   }
 });
