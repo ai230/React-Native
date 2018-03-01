55f958f0a683e4fae76afb1e56c46652e8f9e219
AiYamamoto <ay36230@gmail.com>
2018-02-28 21:20:30 -0800

    update button , list

--- reactnativesample/src/component/TodoItem.js
+++ reactnativesample/src/component/TodoItem.js
@@ -4,6 +4,7 @@ import {
     Text,
     StyleSheet,
 } from 'react-native';
+import Button from './Button'
 
 const styles = StyleSheet.create({
     container: {
@@ -14,10 +15,19 @@ const styles = StyleSheet.create({
       paddingRight: 5,
       marginBottom: 10,
       minHeight: 50,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
     },
     text: {
       color: '#333',
-    }
+    },
+    deleteButton: {
+        backgroundColor: '#800000',
+      },
+      left: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      }
   })
 
   const TodoItem = (props) => {
@@ -27,7 +37,15 @@ const styles = StyleSheet.create({
 
       return (
         <View style={styles.container}>
-            <Text style={styles.text} >{text}</Text>
+            <View style={styles.left}>
+                <Button>
+                    Done
+                </Button>
+                <Text style={styles.text} >{text}</Text>
+                </View>
+                <Button style= {styles.deleteButton}>
+                    Delete
+                </Button>
         </View>
       );
   }
