4370ad2c8ab548f5324e4d13617668ff2ec42a35
AiYamamoto <ay36230@gmail.com>
2018-05-28 02:13:45 +0900

    update

---
+++ redux-calculator/src/containers/CalculatorContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class CalculatorContainer extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                </div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                </div>
+                <div>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                </div>
+            </div>
+        ) 
+    }
+}
+export default CalculatorContainer
